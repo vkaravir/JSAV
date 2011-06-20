5f9357322248986a71d819d7f59553ea9e21ca56
Ville Karavirta <vkaravir@gmail.com>
2011-06-20 21:24:42 +0300

    Removed license. The code is only loaded if Raphael is present.

--- src/graphicals.js
+++ src/graphicals.js
@@ -1,14 +1,8 @@
-/*
- * JSAV 0.0.01 - JavaScript Algorithm Visualization Library
- *
- * Copyright (c) 2011 Ville Karavirta (http://)
- * Licensed under the MIT license.
- */
-
 /**
 * Module that contains the graphical primitive implementations.
 * Depends on core.js, anim.js, jQuery, Raphael
 */
+if (typeof Raphael !== "undefined") { // only execute if Raphael is loaded
 (function($, R) {
  if (typeof JSAV === "undefined") { return; }
  var gp = {};
@@ -28,5 +22,5 @@
    console.log(this);
    return gp.circle(this.getSvg(), x, y, r);
  };
- 
 })(jQuery, Raphael);
+}
