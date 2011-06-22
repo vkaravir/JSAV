2dd7ba84241632e8469fcfa4f78910870522b20d
Ville Karavirta <vkaravir@gmail.com>
2011-06-22 18:08:56 +0300

    Added labels as objects

--- src/graphicals.js
+++ src/graphicals.js
@@ -24,3 +24,57 @@ if (typeof Raphael !== "undefined") { // only execute if Raphael is loaded
  };
 })(jQuery, Raphael);
 }
+
+(function($) {
+  if (typeof JSAV === "undefined") { return; }
+  
+  var Label = function(jsav, text, options) {
+    this.jsav = jsav;
+    this.options = $.extend({display: true}, options);
+    this.element = $('<div class="label" style="display:none;">' + text + '</div>');
+    if (this.options.before) {
+      this.element.insertBefore(this.options.before.element);
+    } else if (this.options.after) {
+      this.element.insertAfter(this.options.before.element);
+    } else {
+      $(this.jsav.container).append(this.element);
+    }
+    if (!(typeof options.display === "boolean" && options.display === true)) {
+      this.show();
+    }    
+  };
+  var labelproto = Label.prototype;
+  labelproto.show = JSAV.anim(
+    function() { this.element.fadeIn(this.jsav.SPEED); }, 
+    function() { this.element.fadeOut(this.jsav.SPEED); }
+  );
+  labelproto.hide = JSAV.anim(
+    function() { this.element.fadeOut(this.jsav.SPEED); }, 
+    function() { this.element.fadeIn(this.jsav.SPEED); }
+  );
+  labelproto._setText = JSAV.anim(
+    function(newText) {
+      this.element.html(newText);
+    }
+  );
+  labelproto.text = function(newValue) {
+    if (typeof newValue === "undefined") {
+      return this.element.html();
+    } else {
+      this._setText(newValue);
+      return this;
+    }
+  };
+  labelproto.state = function(newstate) {
+    if (newstate) {
+      $(this.element).html(newstate);
+    } else {
+      return $(this.element).html();
+    }
+  };
+  
+  JSAV.ext.label = function(text, options) {
+    return new Label(this, text, options);
+  };
+  
+})(jQuery);
