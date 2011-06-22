fb5c1d8e186aaeba2ce11ff24b2094c338082db6
Ville Karavirta <vkaravir@gmail.com>
2011-06-22 21:37:39 +0300

    Added support for indexed array to default layout.

--- src/layout.js
+++ src/layout.js
@@ -4,21 +4,29 @@
 */
 (function() {
   if (typeof JSAV === "undefined") { return; }
-  function getNodeSize(clazz) {
-   
-  }
   function verticalArray(array) {
     var $arr = $(array.element),
-    // rely on browser doing the calculation, float everything to the left
-    $items = $arr.find("li").css({"float": "left", "position":"static"});
+      // rely on browser doing the calculation, float everything to the left..
+      $items = $arr.find("li").css({"float": "left", "position":"static"}),
+      maxHeight = -1,
+      indexed = !!array.options.indexed;
     $items.each(function(index, item) {
       var $i = $(this),
         pos = $i.position();
       $i.css({"left": pos.left - index, "top": pos.top});
+      maxHeight = Math.max(maxHeight, $i.outerHeight());
+      if (indexed) {
+        $arr.addClass("indexed");
+        var $indexLabel = $i.find(".indexlabel");
+        if ($indexLabel.size() === 0) {
+          $i.append('<span class="indexlabel">' + index + '</span>');
+          $indexLabel = $i.find(".indexlabel");
+        }
+      }
     });
-    // and return float and positioning
+    // ..and return float and positioning
     $items.css({"float": "none", "position": "absolute"});
-    $arr.height(60);
+    $arr.height(maxHeight + (indexed?30:0));
   }
   var layouts = {};
   layouts.array = {
