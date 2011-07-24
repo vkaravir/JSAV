9aef99b17985657d7bfae3956b2a0402ccf8b1d6
Cliff Shaffer <shaffer@vt.edu>
2011-07-23 21:13:27 -0400

    minor changes

--- src/anim.js
+++ src/anim.js
@@ -140,7 +140,7 @@
   };
   JSAV.ext.stepdone = function() {return this;};
   JSAV.ext.recorded = function() {
-    console.log("Number of states: ", this._undo.length)
+    console.log("Number of steps: ", this._undo.length)
     this.forward(); // apply the last steps
     this.RECORD = false;
     this.begin();
