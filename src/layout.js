72e9ef47e129be570da239c9c5d4aee31e48deb3
Ville Karavirta <vkaravir@gmail.com>
2011-11-26 22:42:20 +0200

    Added initial version of tree data structure

--- src/layout.js
+++ src/layout.js
@@ -78,11 +78,345 @@
     });
     centerArray(array, $items.last());
   }
+  
+  function treeLayout(tree) {
+	  var NODEGAP = 50;
+    var results = {};
+    var calculateLayout = function(node) {
+  		var ch = node.children();
+  		for (var i = 0; i < ch.length; i++) {
+  			if (ch[i]) {
+  				calculateLayout(ch[i]);
+  			} else {
+  				//debug("child is null!!");
+  			}
+  		}
+  		results[node.id()] = {
+  		  cachedTranslation: {width: 0, height: 0},
+  		  translation: {width: 0, height: 0},
+  		  node: node
+  		};
+      calculateContours(node);
+  	},
+  	calculateContours = function(node) {
+  		var vtcSize = {width: 40, height: 40};
+          var children = node.children();
+          var rootLeft = -vtcSize.width / 2;
+  		var rootRight = vtcSize.width / 2 + (vtcSize.width % 2 === 0 ? 0 : 1);
+  		var rootHeight = vtcSize.height;
+  		if (children.length === 0) {
+  			results[node.id()].contours = new TreeContours(rootLeft, rootRight, rootHeight, node.value());
+  			translateThisNode(node, -rootLeft, 0);
+  		} else {
+  			var transSum = 0;
+  			var firstChild = children[0];
+  			results[node.id()].contours = results[firstChild.id()].contours;
+  			results[firstChild.id()].contours = null;
+  			translateNodes(firstChild, 0, NODEGAP + rootHeight);
+
+  			for (var i = 1; i < children.length; i++) {
+  				var child = children[i];
+  				var childC = results[child.id()].contours;
+  				var trans = results[node.id()].contours.calcTranslation(childC, NODEGAP);
+  				transSum += trans;
+
+  				results[child.id()].contours = null;
+  				results[node.id()].contours.joinWith(childC, trans);
+
+  				translateNodes(child, getXTranslation(firstChild) + trans - getXTranslation(child),
+                                  	NODEGAP + rootHeight);
+  			}
+
+  			var rootTrans = transSum / children.length;
+  			results[node.id()].contours.addOnTop(rootLeft, rootRight, rootHeight, NODEGAP, rootTrans);
+  			translateThisNode(node, getXTranslation(firstChild) + rootTrans, 0);
+  		}
+  	},
+  	translateThisNode = function(node, x, y) {
+  		results[node.id()].translation.width += x;
+  		results[node.id()].translation.height += y;
+  	},
+  	translateAllNodes = function(node, howMuch) {
+  		if (!results[node.id()].cachedTranslation) {
+  			results[node.id()].cachedTranslation = {width: 0, height: 0};
+  		}
+  		results[node.id()].cachedTranslation.width += howMuch.width;
+  		results[node.id()].cachedTranslation.height += howMuch.height;
+  	},
+  	translateNodes = function(node, x, y) {
+  		translateAllNodes(node, {width: x, height: y});
+  	},
+  	getXTranslation = function(node) {
+  		return results[node.id()].translation.width +
+  			((!results[node.id()].cachedTranslation) ? 0 : results[node.id()].cachedTranslation.width);
+  	},
+  	propagateTranslations = function(node) {
+  	  var noderes = results[node.id()];
+  		if (noderes.cachedTranslation) {
+  			var ch = node.children();
+  			for (var i = 0; i < ch.length; i++) {
+  				var child = ch[i];
+  				translateAllNodes(child, noderes.cachedTranslation);
+  				propagateTranslations(child);
+  			}
+  			noderes.translation.width += noderes.cachedTranslation.width;
+  			noderes.translation.height += noderes.cachedTranslation.height;
+  			noderes.cachedTranslation = null;
+  		}
+  	},
+  	calculateFinalLayout = function(node, dx, dy) {
+  	        if (-results[node.id()].contours.cLeftExtent - getXTranslation(node) > 0) {
+  			translate(node, -results[node.id()].contours.cLeftExtent - this.getXTranslation(node), 0);
+  		}
+  		translateNodes(node, dx, dy);
+  		propagateTranslations(node);
+  	};
+  	
+  	calculateLayout(tree.root());
+  	calculateFinalLayout(tree.root(), 20, 10+NODEGAP);
+  	$.each(results, function(key, value) {
+  	  var oldPos = value.node.element.position();
+  	  if (oldPos.left == 0 && oldPos.top == 0) {
+    	  value.node.element.css({left: value.translation.width + "px", top: value.translation.height + "px"});
+  	  } else {
+    	  value.node.css({left: value.translation.width + "px", top: value.translation.height + "px"});
+  	  }
+  	});
+  	var offset = tree.element.position();
+  	$.each(results, function(key, value) {
+  	  var node = value.node;
+  	  if (node['edgetoparent']) {
+  	    var start = {left: value.translation.width + offset.left,
+  	                 top: value.translation.height + offset.top},
+  	        endnode = results[node.parent().id()].translation,
+  	        end = {left: endnode.width + offset.left,
+  	               top: endnode.height + offset.top};
+  	        
+  	    edgeLayout(node.edgetoparent, start, end);
+  	  }
+  	});
+  }
+  
+  var edgeLayout = function(edge, start, end) {
+    var NODESIZE = edge.startnode.element.outerWidth()/2.0,
+      svgstyle = edge.jsav.getSvg().canvas.style;
+    var startpos = edge.startnode.element.offset(),
+        endpos = edge.endnode.element.offset(),
+        fromX =  Math.round(start.left + NODESIZE - parseInt(svgstyle.left, 10)),
+  	    fromY = Math.round(start.top + NODESIZE - parseInt(svgstyle.top, 10)),
+  	    toX = Math.round(end.left + NODESIZE - parseInt(svgstyle.left, 10)),
+  	    toY = Math.round(end.top + NODESIZE - parseInt(svgstyle.top, 10)),
+  	    fromAngle = normalizeAngle(2*Math.PI - Math.atan2(toY - fromY, toX - fromX)),
+        toAngle = normalizeAngle(2*Math.PI - Math.atan2(fromY - toY, fromX - toX)),
+        fromPoint = getNodeBorderAtAngle(0, edge.startnode.element, 
+                  {width: NODESIZE, height: NODESIZE, x: fromX, y: fromY}, fromAngle),
+        toPoint = getNodeBorderAtAngle(1, edge.endnode.element, 
+                  {width: NODESIZE, height: NODESIZE, x: toX, y: toY}, toAngle)
+    edge.g.movePoints([fromPoint, toPoint]);
+
+    function normalizeAngle(angle) {
+    	while (angle < 0)
+        angle += 2 * Math.PI;
+      while (angle >= 2 * Math.PI) 
+        angle -= 2 * Math.PI;
+      return angle;
+    };
+    function getNodeBorderAtAngle(pos, node, dim, angle) {
+      // dim: x, y coords of center and half of width and height
+    	var x, y,
+          urCornerA = Math.atan2(dim.height*2.0, dim.width*2.0),
+          ulCornerA = Math.PI - urCornerA,
+          lrCornerA = 2*Math.PI - urCornerA,
+          llCornerA = urCornerA + Math.PI;
+
+      if (angle < urCornerA || angle > lrCornerA) { // on right side
+        x = dim.x + dim.width;
+        y = dim.y - (dim.width) * Math.tan(angle);
+      } else if (angle > ulCornerA && angle < llCornerA) { // left
+        x = dim.x - dim.width;
+        y = dim.y + (dim.width) * Math.tan(angle - Math.PI);
+      } else if (angle <= ulCornerA) { // top
+        x = dim.x + (dim.height) / Math.tan(angle);
+        y = dim.y- dim.height;
+      } else { // on bottom side
+        x = dim.x - (dim.height) / Math.tan(angle - Math.PI);
+        y = dim.y + dim.height;
+      }
+    	return [pos, Math.round(x), Math.round(y)];
+    }
+  }
+  
   var layouts = {};
   layouts.array = {
     "_default": verticalArray,
     "bar": barArray,
     "array": verticalArray
   };
+  layouts.tree = {
+    "_default": treeLayout
+  };
+  layouts.edge = {
+    "_default": edgeLayout
+  };
   JSAV.ext.layout = layouts;
 })();
+
+TreeContours = function(left, right, height, data) {
+		this.cHeight = height;
+		this.leftCDims = [];
+		this.leftCDims[this.leftCDims.length] = {width: -left, height: height};
+		this.cLeftExtent = left;
+		this.rightCDims = [];
+		this.rightCDims[this.rightCDims.length] = {width: -right, height: height};
+		this.cRightExtent = right;
+	};
+TreeContours.prototype = {
+	getHeight: function() {
+		return this.cHeight;
+	},
+	getWidth: function() {
+		return this.cRightExtent - this.cLeftExtent;
+	},
+	addOnTop: function(left, right, height, addHeight, originTrans) {
+	  var lCD = this.leftCDims,
+	      rCD = this.rightCDims;
+		lCD[lCD.length-1].height += addHeight;
+		lCD[lCD.length-1].width += originTrans + left;
+		rCD[rCD.length-1].height += addHeight;
+		rCD[rCD.length-1].width += originTrans + right;
+
+		lCD.push({width: -left, height: height});
+		rCD.push({width: -right, height: height});
+		this.cHeight += height + addHeight;
+		this.cLeftExtent -= originTrans;
+		this.cRightExtent -= originTrans;
+		if (left < this.cLeftExtent) {
+			this.cLeftExtent = left;
+		}
+		if (right > this.cRightExtent) {
+			this.cRightExtent = right;
+		}
+	},
+	joinWith: function(other, hDist) {
+		if (other.cHeight > this.cHeight) {
+			var newLeftC = new Array();
+			var otherLeft = other.cHeight - this.cHeight;
+			var thisCDisp = 0;
+			var otherCDisp = 0;
+			$.each(other.leftCDims, function (index, item) {
+				if (otherLeft > 0 ) {
+					var dim = {width: item.width, height: item.height};
+					otherLeft -= item.height;
+					if (otherLeft < 0) {
+						dim.height += otherLeft;					
+					}
+					newLeftC[newLeftC.length] = dim;
+				} else {
+					otherCDisp += item.width;
+				}
+			});
+			var middle = newLeftC[newLeftC.length - 1];
+
+			$.each(this.leftCDims, function(index, item) {
+				thisCDisp += item.width;
+				newLeftC[newLeftC.length] = {width: item.width, height: item.height};
+			});
+               
+			middle.width -= thisCDisp - otherCDisp;
+			middle.width -= hDist;
+			this.leftCDims = newLeftC;
+		}
+		if (other.cHeight >= this.cHeight) {
+			this.rightCDims = other.rightCDims.slice();
+		} else {
+			var thisLeft = this.cHeight - other.cHeight;
+			var nextIndex = 0;
+
+			var thisCDisp = 0;
+			var otherCDisp = 0;
+			$.each(this.rightCDims, function (index, item) {
+				if (thisLeft > 0 ) {
+					nextIndex++;
+					thisLeft -= item.height;
+					if (thisLeft < 0) {
+						item.height += thisLeft;
+					}
+				} else {
+					thisCDisp += item.width;
+				}
+			});
+			for (var i = nextIndex+1;i< this.rightCDims.length;i++) {
+				this.rightCDims[i] = null;
+			}
+			this.rightCDims = this.rightCDims.compact();
+			var middle = this.rightCDims[nextIndex];
+
+			for (i = 0; i < other.rightCDims.length; i++) {
+				var item = other.rightCDims[i];
+				otherCDisp += item.width;
+				this.rightCDims[this.rightCDims.length] = {width: item.width, height: item.height};
+			}
+			middle.width += thisCDisp - otherCDisp;
+			middle.width += hDist;
+		}
+		this.rightCDims[this.rightCDims.length-1].width -= hDist;
+
+		if (other.cHeight > this.cHeight) {
+			this.cHeight = other.cHeight;
+		}
+		if (other.cLeftExtent + hDist < this.cLeftExtent) {
+			this.cLeftExtent = other.cLeftExtent + hDist;
+		}
+		if (other.cRightExtent + hDist > this.cRightExtent) {
+			this.cRightExtent = other.cRightExtent + hDist;
+		}
+	},
+	calcTranslation: function(other, wantedDist) {
+		var lc = this.rightCDims,
+		    rc = other.leftCDims,
+		    li = lc.length - 1,
+		    ri = rc.length - 1,
+        lCumD = {width: 0, height: 0},
+		    rCumD = {width: 0, height: 0},
+		    displacement = wantedDist;
+
+		while (true) {
+			if (li < 0) {
+				if (ri < 0 || rCumD.height >= lCumD.height) {
+					break;
+				}
+				var rd = rc[ri];
+				rCumD.height += rd.height;
+				rCumD.width += rd.width;
+				ri--;
+			} else if (ri < 0) {
+				if (lCumD.height >= rCumD.height) {
+					break;
+				}
+				var ld = lc[li];
+				lCumD.height += ld.height;
+				lCumD.width += ld.width;
+				li--;
+			} else {
+				var ld = lc[li];
+				var rd = rc[ri];
+				var leftNewHeight = lCumD.height;
+				var rightNewHeight = rCumD.height;
+				if (leftNewHeight <= rightNewHeight) {
+					lCumD.height += ld.height;
+					lCumD.width += ld.width;
+					li--;
+				}
+				if (rightNewHeight <= leftNewHeight) {
+					rCumD.height += rd.height;
+					rCumD.width += rd.width;
+					ri--;
+				}
+			}
+			if (displacement < rCumD.width - lCumD.width + wantedDist) {
+				displacement = rCumD.width - lCumD.width + wantedDist;
+			}
+		}
+		return displacement;
+	}
+};
---
+++ src/tree.js
@@ -0,0 +1,315 @@
+/**
+* Module that contains the tree data structure implementations.
+* Depends on core.js, datastructures.js, anim.js
+*/
+(function($) {
+  if (typeof JSAV === "undefined") { return; }
+
+  var _setcss = function(cssprop, value) {
+    var oldProps = $.extend(true, {}, cssprop),
+        el = this.element,
+        newprops;
+    if (typeof cssprop === "string" && typeof value !== "undefined") {
+      oldProps[cssprop] = el.css(cssprop);
+      newprops = {};
+      newprops[cssprop] = value;
+    } else {
+      for (var i in cssprop) {
+        oldProps[i] = el.css(i);
+      }
+      newprops = cssprop;
+    }
+    if (!this.jsav.RECORD && !$.fx.off) { // only animate when playing, not when recording
+      this.element.animate(newprops, this.jsav.SPEED);
+    } else {
+      this.element.css(newprops);
+    }
+    return [oldProps];
+  };
+  
+  var Tree = function(jsav, options) {
+    this.jsav = jsav;
+    this.options = options;
+    this.root = null;
+  };
+  var treeproto = Tree.prototype;
+  JSAV.ext.ds.extend("common", treeproto);
+  treeproto.root = function(newRoot) {
+    if (typeof newRoot === "undefined") {
+      return this.rootnode;
+    }
+    this.rootnode.value(newRoot);
+  };
+  treeproto.newNode = function(value) {
+    return new TreeNode(this.jsav, value);
+  };
+  treeproto.height = function() {
+    return this.root.height();
+  };
+  treeproto.layout = function() {
+    var layoutAlg = this.options.layout || "_default";
+    this.jsav.layout.tree[layoutAlg](this);
+  };
+  treeproto.equals = function(otherTree, options) {
+    if (!otherTree instanceof Tree) {
+      return false;
+    }
+    return this.rootnode.equals(otherTree.rootnode, options);
+  };
+  treeproto._setcss = JSAV.anim(_setcss);
+  treeproto.css = function(cssprop) {
+    if (typeof cssprop === "string") {
+      return $elems.css(cssprop);
+    } else {
+      return this._setcss(cssprop);
+    }
+  };
+  treeproto.state = function() {
+    // TODO: Should tree.state be implemented??? Probably..
+  };
+  
+  var TreeNode = function(container, value, parent, options) {
+    this.jsav = container.jsav;
+    this.container = container;
+    this.parentnode = parent;
+    this.options = $.extend(true, {display: true}, options);
+    var el = $("<div>" + value + "</div>").addClass("jsavnode jsavtreenode jsavbinarynode")
+              .attr("data-value", value);
+    this.element = el;
+    this.container.element.append(el);
+    el.css("display", "hidden");
+    var visible = (typeof this.options.display === "boolean" && this.options.display === true);
+    if (visible) {
+      if (jsav.currentStep() === 0) { // at beginning, just make it visible
+        el.css("display", "block");
+      } else { // add effect to show otherwise
+        this.show();
+      }
+    }
+    if (parent) {
+      this.edgetoparent = new Edge(this.jsav, this, parent);
+    }
+    this.childnodes = [];
+  };
+  var nodeproto = TreeNode.prototype;
+  JSAV.ext.ds.extend("common", nodeproto);
+  nodeproto.value = function(newVal) {
+    if (typeof newVal === "undefined") {
+      return this.element.attr("data-value");
+    } else {
+      this._setvalue(newVal);
+    }
+    return this;
+  };
+  nodeproto._setvalue = JSAV.anim(function(newValue) {
+    var oldVal = this.element.attr("data-value") || "";
+    this.element.text(newValue).attr("data-value", newValue);
+    return [oldVal];
+  });
+  nodeproto.parent = function() {
+    return this.parentnode;
+  };
+  nodeproto.edgeToParent = function(edge) {
+    if (typeof edge === "undefined") {
+      return this.edgetoparent;
+    } else {
+      this.edgetoparent = edge;
+      return this;
+    }
+  };
+  nodeproto.addChild = function(node) {
+    this.childnodes.push(node);
+    return this;
+  }
+  nodeproto.child = function(pos, node) {
+    if (typeof node === "undefined") {
+      return this.childnodes[pos];
+    } else {
+      this.childnodes[pos] = node;
+    }
+  };
+  nodeproto.children = function() {
+    return this.childnodes;
+  }
+  nodeproto.state = function() {
+    // TODO: Should this be implemented??? Probably..
+  };
+  nodeproto._setcss = JSAV.anim(_setcss);
+  nodeproto.css = function(cssprop, value) {
+    if (typeof cssprop === "string" && typeof value === "undefined") {
+      return this.element.css(cssprop);
+    } else {
+      return this._setcss(cssprop, value);
+    }
+  };
+  
+  var Edge = function(jsav, start, end, options) {
+    this.jsav = jsav;
+    this.startnode = start;
+    this.endnode = end;
+    this.options = $.extend(true, {"display": true}, options);
+    this.container = start.container;
+    this.g = this.jsav.g.line(start.element.offset().left,
+                              start.element.offset().top,
+                              end.element.offset().left,
+                              end.element.offset().top);
+
+    var visible = (typeof this.options.display === "boolean" && this.options.display === true);
+    this.g.rObj.attr({"opacity": 0});
+    if (visible) {
+      if (this.jsav.currentStep() === 0) { // at beginning, just make it visible
+        this.g.rObj.attr({"opacity": 1});
+      } else { // add effect to show otherwise
+        this.g.show();
+      }
+    }
+    
+  };
+  var edgeproto = Edge.prototype;
+  JSAV.ext.ds.extend("common", edgeproto);
+  edgeproto.start = function(node) {
+    if (typeof node === "undefined") {
+      return this.startnode;
+    } else {
+      this.startnode = node;
+      return this;
+    }
+  };
+  edgeproto.end = function(node) {
+    if (typeof node === "undefined") {
+      return this.endnode;
+    } else {
+      this.endnode = node;
+      return this;
+    }
+  };
+  edgeproto.weight = function(node) {
+    
+  };
+  
+  var BinaryTree = function(jsav, options) {
+    this.jsav = jsav;
+    this.options = options;
+    var el = this.options.element || $("<div/>");
+    el.addClass("jsavtree jsavbinarytree");
+    if (!this.options.element) {
+      $(this.jsav.container).append(el);
+    }
+    this.element = el;
+    this.rootnode = this.newNode("");
+    //this.layout();
+    el.css("display", "hidden");
+    var visible = (typeof this.options.display === "boolean" && this.options.display === true);
+    if (visible) {
+      if (this.jsav.currentStep() === 0) { // at beginning, just make it visible
+        el.css("display", "block");
+      } else { // add effect to show otherwise
+        this.show();
+      }
+    }
+  };
+  var bintreeproto = BinaryTree.prototype;
+  $.extend(bintreeproto, treeproto);
+  bintreeproto.newNode = function(value, parent) {
+    return new BinaryTreeNode(this, value, parent || this.rootnode);
+  };
+  
+  var BinaryTreeNode = function(container, value, parent, options) {
+    this.jsav = container.jsav;
+    this.container = container;
+    this.parentnode = parent;
+    this.options = $.extend(true, {display: true}, options);
+    var el = $("<div>" + value + "</div>").addClass("jsavnode jsavtreenode jsavbinarynode")
+              .attr("data-value", value);
+    this.element = el;
+    this.container.element.append(el);
+    el.css("display", "hidden");
+    var visible = (typeof this.options.display === "boolean" && this.options.display === true);
+    if (visible) {
+      if (jsav.currentStep() === 0) { // at beginning, just make it visible
+        el.css("display", "block");
+      } else { // add effect to show otherwise
+        this.show();
+      }
+    }
+    if (parent) {
+      this.edgetoparent = new Edge(this.jsav, this, parent);
+    }
+  };
+  var binnodeproto = BinaryTreeNode.prototype;
+  $.extend(binnodeproto, nodeproto);
+
+  binnodeproto.children = function() {
+    if (this.leftnode && this.rightnode) {
+      return [this.leftnode, this.rightnode];
+    } else if (this.leftnode) {
+      return [this.leftnode];
+    } else if (this.rightnode) {
+      return [this.rightnode];
+    } else {
+      return [];
+    }
+  };
+  binnodeproto.left = function(node) {
+    if (typeof node === "undefined") {
+      return this.leftnode;
+    } else if (node.constructor === BinaryTreeNode) {
+      this.leftnode = node;
+    } else {
+      if (this.leftnode) {
+        this.leftnode.value(node);
+      } else {
+        this.leftnode = new BinaryTreeNode(this.container, node, this);
+        //this.container.layout();
+      }
+    }
+    return this.leftnode;
+  };
+  binnodeproto.right = function(node) {
+    if (typeof node === "undefined") {
+      return this.rightnode;
+    } else if (node.constructor === BinaryTreeNode) {
+      this.rightnode = node;
+    } else {
+      this.rightnode = new BinaryTreeNode(this.container, node, this);
+      //this.container.layout();
+    }
+    return this.rightnode;
+  };
+  
+  var BinarySearchTree = function(jsav, options) {
+    this.jsav = jsav;
+  };
+  var bstproto = BinarySearchTree.prototype;
+  $.extend(bstproto, bintreeproto);
+  bstproto.insert = function(value) {
+    // TODO: implement BST insert
+  };
+  bstproto.remove = function(value) {
+    // TODO: implement BST delete
+  };
+  bstproto.search = function(value) {
+    // TODO: implement BST search
+  };
+  
+  var dstypes = JSAV._types.ds;
+  dstypes.Tree = Tree.prototype;
+  dstypes.TreeNode = TreeNode.prototype;
+  dstypes.Edge = Edge.prototype;
+  dstypes.BinaryTree = BinaryTree.prototype;
+  dstypes.BinarySearchTree = BinarySearchTree.prototype;
+  dstypes.BinaryTreeNode = BinaryTreeNode.prototype;
+
+  JSAV.ext.ds.tree = function(options) {
+    return new Tree(this, $.extend(true, {}, options));
+  };
+  JSAV.ext.ds.bintree = function(options) {
+    return new BinaryTree(this, $.extend(true, {}, options));
+  };
+  JSAV.ext.ds.bst = function(options) {
+    return new BinarySearchTree(this, $.extend(true, {}, options));
+  };
+  JSAV.ext.ds.edge = function(options) {
+    return new Edge(this, $.extend(true, {}, options));
+  };
+})(jQuery);
