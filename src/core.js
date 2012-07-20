05c0a1094e9a11c66daadd1604bfa50a2bef09ea
Ville Karavirta <vkaravir@gmail.com>
2012-07-20 11:17:18 +0300

    Removed call to raphael.renderfix() for now

--- src/core.js
+++ src/core.js
@@ -18,7 +18,7 @@
   jsavproto.getSvg = function() {
     if (!this.svg) { // lazily create the SVG overlay only when needed
       this.svg = Raphael(this.canvas[0]);
-      this.svg.renderfix();
+//      this.svg.renderfix();
       var style = this.svg.canvas.style;
       style.position = "absolute";
     }
