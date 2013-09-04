2eede9826f58c352b3f9631a3090e0d3b14fedea
Ville Karavirta <vkaravir@gmail.com>
2013-09-04 21:39:15 +0300

    Updated graph tests to match previous change (see #100)

--- test/unit/graph.js
+++ test/unit/graph.js
@@ -15,14 +15,14 @@
         e = graph.addNode("E"),
         f = graph.addNode("F");
     equal(graph.nodeCount(), 6);
-    graph.addEdge(a, b);
+    var e1 = graph.addEdge(a, b);
     graph.addEdge(b, a);
     graph.addEdge(a, c);
     graph.addEdge(b, d);
     graph.addEdge(e, a);
     graph.addEdge(d, e);
     graph.addEdge(d, f);
-    equal(graph.edgeCount(), 12);
+    equal(graph.edgeCount(), 6);
     ok(graph.hasEdge(a, b));
     ok(graph.hasEdge(b, a));
     ok(graph.hasEdge(a, c));
@@ -41,6 +41,8 @@
     equal(d.neighbors().length, 3);
     equal(e.neighbors().length, 2);
     equal(f.neighbors().length, 1);
+    equal(graph.getEdge(a, b), e1);
+    equal(graph.getEdge(b, a).id(), e1.id());
   });
 
   test("Graph edge removal", function() {
@@ -54,43 +56,43 @@
         e2 = graph.addEdge(b, c),
         e3 = graph.addEdge(c, d),
         e4 = graph.addEdge(d, a);
-    equal(graph.edges().length, 8);
-    equal(graph.edgeCount(), 8);
+    equal(graph.edges().length, 4);
+    equal(graph.edgeCount(), 4);
     av.displayInit();
     graph.removeEdge(e1);
     graph.layout();
-    equal(graph.edges().length, 6);
-    equal(graph.edgeCount(), 6);
+    equal(graph.edges().length, 3);
+    equal(graph.edgeCount(), 3);
     ok(!e1.isVisible());
     av.step();
     graph.removeEdge(graph.getEdge(c, b));
     graph.layout();
-    equal(graph.edges().length, 4);
-    equal(graph.edgeCount(), 4);
+    equal(graph.edges().length, 2);
+    equal(graph.edgeCount(), 2);
     ok(!e2.isVisible());
     av.step();
     graph.removeEdge(e4);
     graph.layout();
-    equal(graph.edges().length, 2);
-    equal(graph.edgeCount(), 2);
+    equal(graph.edges().length, 1);
+    equal(graph.edgeCount(), 1);
     ok(!e4.isVisible());
     av.recorded();
     $.fx.off = true;
 
-    equal(graph.edges().length, 8);
-    equal(graph.edgeCount(), 8);
-    ok(e1.isVisible() && e2.isVisible() && e4.isVisible());
-    av.forward();
-    equal(graph.edges().length, 6);
-    equal(graph.edgeCount(), 6);
-    ok(!e1.isVisible() && e2.isVisible() && e4.isVisible());
-    av.forward();
     equal(graph.edges().length, 4);
     equal(graph.edgeCount(), 4);
-    ok(!e1.isVisible() && !e2.isVisible() && e4.isVisible());
+    ok(e1.isVisible() && e2.isVisible() && e4.isVisible());
+    av.forward();
+    equal(graph.edges().length, 3);
+    equal(graph.edgeCount(), 3);
+    ok(!e1.isVisible() && e2.isVisible() && e4.isVisible());
     av.forward();
     equal(graph.edges().length, 2);
     equal(graph.edgeCount(), 2);
+    ok(!e1.isVisible() && !e2.isVisible() && e4.isVisible());
+    av.forward();
+    equal(graph.edges().length, 1);
+    equal(graph.edgeCount(), 1);
     ok(!e1.isVisible() && !e2.isVisible() && !e4.isVisible());
   });
 })();
